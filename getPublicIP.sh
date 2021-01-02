8102697a108cbaf675bc4d743579d9a0a24a3c7a
SaxonLW <saxon_lw@outlook.com>
2021-01-02 12:53:38 +1100

    114.73.180.69

--- getPublicIP.sh
+++ getPublicIP.sh
@@ -3,11 +3,12 @@ PublicIP=`curl ipinfo.io/ip -s`
 IzzyIP="$PublicIP:25565"
 BoizIP="$PublicIP:25566"
 TimeStamp=`date +%s`
+epochLink="[html]<a href=""[html]https://www.epochconverter.com/?q=$TimeStamp"">$TimeStamp</a>"
 
 echo "$PublicIP" > Coup.ip
 echo "$IzzyIP" > IzzyMinecraft.ip
 echo "$BoizIP" > BoizMinecraft.ip
-echo "https://www.epochconverter.com/?q=$TimeStamp" > LastRunAt.epoch
+echo "epochLink" > LastRunAt.html
 
 git add --all
 git commit -m "$PublicIP" --quiet
