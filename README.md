45209f8ee08ae044747cdc41948c22cbd720f79f
SaxonLW <saxon_lw@outlook.com>
2021-01-02 13:07:43 +1100

    114.73.180.69

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #Coup Public IP List
-[Last Refreshed : 1609553162](<html><a href=https://www.epochconverter.com/?q=1609553162>1609553162</a></html>)
+[Last Refreshed : 1609553263](<html><a href=https://www.epochconverter.com/?q=1609553263>1609553263</a></html>)
--- getPublicIP.sh
+++ getPublicIP.sh
@@ -4,7 +4,8 @@ IzzyIP="$PublicIP:25565"
 BoizIP="$PublicIP:25566"
 TimeStamp=`date +%s`
 EpochLink="<html><a href=""https://www.epochconverter.com/?q=$TimeStamp"">$TimeStamp</a></html>"
-READMEmsg="#Coup Public IP List\n[Last Refreshed : $TimeStamp]($EpochLink)"
+READMEmsg="#Coup Public IP List
+[Last Refreshed : $TimeStamp]($EpochLink)"
 
 echo "$PublicIP" > Coup.ip
 echo "$IzzyIP" > IzzyMinecraft.ip
